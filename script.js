68a16434ab64b76a03513b59454c041aff441465
Kunj-gondaliya <gondaliyakunj71@gmail.com>
2025-09-05 21:37:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// Contact form
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("contactForm");
   if (form) {
     form.addEventListener("submit", (e) => {
       e.preventDefault();
-      alert("✅ Thank you for contacting Webora! We'll reply soon.");
+      alert("Thank you for contacting Webora! We'll get back to you soon.");
       form.reset();
     });
   }
