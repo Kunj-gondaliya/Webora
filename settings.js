391d8a66633124bf5602dfde6b2491efe62811a5
Kunj-gondaliya <gondaliyakunj71@gmail.com>
2025-09-20 16:13:24 +0530

    update

--- settings.js
+++ settings.js
@@ -1,16 +1,24 @@
 const toggleSwitch = document.getElementById("darkModeToggle");
-const profileNameInput = document.getElementById("profileName");
+const profileNameInput = document.getElementById("profileNameDisplay");
+const ageNameInput = document.getElementById("ageNameDisplay");
 const saveBtn = document.querySelector(".save-btn");
 
+
 // Load saved profile name
-if (localStorage.getItem("profileName")) {
-  profileNameInput.value = localStorage.getItem("profileName");
+if (localStorage.getItem("profileNameDisplay")) {
+  profileNameInput.value = localStorage.getItem("profileNameDisplay");
+}
+if (localStorage.getItem("ageNameDisplay")) {
+  ageNameInput.value = localStorage.getItem("ageNameDisplay");
 }
 
 // Save profile name
 saveBtn.addEventListener("click", () => {
-  localStorage.setItem("profileName", profileNameInput.value);
+  localStorage.setItem("profileNameDisplay", profileNameInput.value);
   alert("Profile name saved!");
+
+  localStorage.setItem("ageNameDisplay", ageNameInput.value);
+  alert("AGE saved!");
 });
 
 // Load saved theme
